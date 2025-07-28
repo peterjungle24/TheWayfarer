eb1a92d7591446fd339e58da4c1e50deeebaad7d
Pedro <peterjungle20@gmail.com>
2025-07-28 15:57:08 -0300

    Added a "One Arm" mechanic, still kinda glitchy.. Ghost hand be like: I also added the mod folder on root. Its working nice *(to me atleast)*
    All arts and drawings are just placeholders. Just ignore them for now
    The code matters

---
+++ Code/Globals.cs
@@ -0,0 +1,35 @@
+﻿// i learned! :monksilly:
+
+// System | Unity
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Runtime.CompilerServices;
+global using UnityEngine;
+global using MonoMod.Cil;
+global using static UnityEngine.Random;
+
+// BepInEx
+global using BepInEx;
+global using BepInEx.Logging;
+
+// Slugbase (nescesary for that json file)
+global using SlugBase;
+global using SlugBase.Config;
+
+// Rain World ones
+global using MoreSlugcats;
+global using DevInterface;
+global using Menu.Remix.MixedUI;
+global using RWCustom;
+global using objPhy = AbstractPhysicalObject;
+global using objType = AbstractPhysicalObject.AbstractObjectType;
+global using DLC_ObjType = DLCSharedEnums.AbstractObjectType;
+global using PlayerGrab = Player.ObjectGrabability;
+
+// Mods Libraries
+global using static Pom.Pom;
+
+// My Namespaces
--- Code/Plugin.cs
+++ Code/Plugin.cs
@@ -1,12 +1,11 @@
-﻿using UnityEngine;
-using BepInEx;
-using BepInEx.Logging;
-
-namespace Code
+﻿namespace Code
 {
     [BepInPlugin(ID, NAME, VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        // the Wayfare scug id
+        public static readonly SlugcatStats.Name slgWayfare = new SlugcatStats.Name("theresAFareOnMyWay");
+
         // cool thing that logs to "mods.log" file (with LogManager)
         public static ManualLogSource logger;
         // needs to match with "modinfo.json" properties
@@ -20,6 +19,45 @@ namespace Code
             logger = base.Logger;
 
             On.RainWorld.OnModsInit += Initialize;
+
+            // removing arm
+            On.PlayerGraphics.DrawSprites += RemovesArm;
+            On.Player.Update += Player_Update;
+            On.Player.Grabability += Player_Grabability;
+        }
+
+        private Player.ObjectGrabability Player_Grabability(On.Player.orig_Grabability orig, Player self, PhysicalObject obj)
+        {
+            if (self.slugcatStats.name == slgWayfare)
+            {
+                PlayerGrab result = orig.Invoke(self, obj);
+                result = Grabability(self, result);
+
+                return result;
+            }
+
+            return orig(self, obj);
+        }
+        private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            if (self.slugcatStats.name == slgWayfare)
+            {
+                if (self.grasps[0] != null && self.grasps[1] != null)
+                {
+                    self.ReleaseGrasp(1);
+                }
+            }
+
+            orig(self, eu);
+        }
+        private void RemovesArm(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            orig(self, sLeaser, rCam, timeStacker, camPos);
+            if (self.player.slugcatStats.name == slgWayfare)
+            {
+                sLeaser.sprites[6].isVisible = false;
+                sLeaser.sprites[8].isVisible = false;
+            }
         }
 
         /// <summary>
@@ -33,6 +71,25 @@ namespace Code
             logger.LogInfo("Wayfare is here! Literally.");
 
             orig(self);
+        }
+
+        /***********************************************************/
+        public Player.ObjectGrabability Grabability(Player self, PlayerGrab result)
+        {
+            PlayerGrab result2;
+            
+            result2 = result;
+            if (result == PlayerGrab.OneHand) result = PlayerGrab.BigOneHand;
+            else
+            {
+                if (result == PlayerGrab.BigOneHand) result = PlayerGrab.BigOneHand;
+                else
+                    if (result == PlayerGrab.TwoHands) result = PlayerGrab.Drag;
+                    else if (result == PlayerGrab.Drag) result = PlayerGrab.Drag;
+            }
+            result2 = result;
+
+            return result2;
         }
     }
 }
