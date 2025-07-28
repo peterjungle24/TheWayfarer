23d1b4410e309632a0790e29ce97a057a9318e4a
Pedro <peterjungle20@gmail.com>
2025-07-28 16:35:12 -0300

    Just updated : Fixed the ghost hand to switch the hand and also did some tweaks about the slugcat in arena. idk why their eyes is still invisible, next commit i will fix them

--- Code/Plugin.cs
+++ Code/Plugin.cs
@@ -24,7 +24,22 @@
             On.PlayerGraphics.DrawSprites += RemovesArm;
             On.Player.Update += Player_Update;
             On.Player.Grabability += Player_Grabability;
+            On.Player.FreeHand += Player_FreeHand;
         }
+
+        private int Player_FreeHand(On.Player.orig_FreeHand orig, Player self)
+        {
+            int result = orig.Invoke(self);
+
+            if (self.grasps[0] != null || self.grasps[1] != null)
+            {
+                result = -1;
+            }
+
+            return result;
+        }
+
+        #region One Arm
 
         private Player.ObjectGrabability Player_Grabability(On.Player.orig_Grabability orig, Player self, PhysicalObject obj)
         {
@@ -52,13 +67,61 @@
         }
         private void RemovesArm(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
+            /*
             orig(self, sLeaser, rCam, timeStacker, camPos);
             if (self.player.slugcatStats.name == slgWayfare)
             {
+                // Hand 1
+                sLeaser.sprites[5].isVisible = false;
+                sLeaser.sprites[7].isVisible = false;
+
+                // Hand 2
                 sLeaser.sprites[6].isVisible = false;
                 sLeaser.sprites[8].isVisible = false;
             }
+            */
+
+            orig.Invoke(self, sLeaser, rCam, timeStacker, camPos);
+            
+            DrawSpritesHandler(self.player, sLeaser, rCam, timeStacker, camPos);
         }
+
+        /***********************************************************/
+        public Player.ObjectGrabability Grabability(Player self, PlayerGrab result)
+        {
+            PlayerGrab result2;
+
+            result2 = result;
+            if (result == PlayerGrab.OneHand) result = PlayerGrab.BigOneHand;
+            else
+            {
+                if (result == PlayerGrab.BigOneHand) result = PlayerGrab.BigOneHand;
+                else
+                    if (result == PlayerGrab.TwoHands) result = PlayerGrab.Drag;
+                else if (result == PlayerGrab.Drag) result = PlayerGrab.Drag;
+            }
+            result2 = result;
+
+            return result2;
+        }
+        public void DrawSpritesHandler(Player player, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            if (player.graphicsModule != null || sLeaser != null)
+            {
+                PlayerGraphics self = player.graphicsModule as PlayerGraphics;
+                int grasp = 0;
+
+                if (player.grasps[1] != null) grasp = 1;
+                if (sLeaser.sprites.Length >= 9)
+                {
+                    for (int i = 5; i <= 8; i++)
+                        if (i == 6 - grasp || i == 8 - grasp)
+                            sLeaser.sprites[i].isVisible = false;
+                }
+            }
+        }
+
+        #endregion
 
         /// <summary>
         /// called when the mods are initializing
@@ -71,25 +134,6 @@
             logger.LogInfo("Wayfare is here! Literally.");
 
             orig(self);
-        }
-
-        /***********************************************************/
-        public Player.ObjectGrabability Grabability(Player self, PlayerGrab result)
-        {
-            PlayerGrab result2;
-            
-            result2 = result;
-            if (result == PlayerGrab.OneHand) result = PlayerGrab.BigOneHand;
-            else
-            {
-                if (result == PlayerGrab.BigOneHand) result = PlayerGrab.BigOneHand;
-                else
-                    if (result == PlayerGrab.TwoHands) result = PlayerGrab.Drag;
-                    else if (result == PlayerGrab.Drag) result = PlayerGrab.Drag;
-            }
-            result2 = result;
-
-            return result2;
         }
     }
 }
